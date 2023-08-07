e3a800819f79f7d5953b6af04238fce0fffdaeba
unknown <necmeddinxelilzade2002@gmail.com>
2023-08-07 11:12:57 +0400

    some changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,54 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import {Link, Outlet} from "react-router-dom"; 
-import styles from './Navbar.module.scss'
-import {UserOutlined} from "@ant-design/icons";
+import React, { useEffect, useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import styles from "./Navbar.module.scss";
+import { UserOutlined } from "@ant-design/icons";
 
 const Navbar = () => {
-  const[isDark,setIsDark]=useState(false)
+  const [isDark, setIsDark] = useState(false);
 
-  const handleChangeInput=(e)=>{
-     const isChecked = e.target.checked;
-     setIsDark(isChecked);
-  }
+  const handleChangeInput = (e) => {
+    const isChecked = e.target.checked;
+    setIsDark(isChecked);
+  };
 
-  useEffect(()=>{
-    if (isDark){
-      document.body.classList.add('dark');
-    }else{
-      document.body.classList.remove('dark');
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
     }
-  },[isDark])
+  }, [isDark]);
 
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.route}>
+          <Link to="/help">Helps</Link>
+        </div>
 
+        <div className={styles.route}>
+          <Link to="/blog">Blog-page</Link>
+        </div>
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.left}>
-                <div className={styles.route} >
-                   <Link to='/help'>Helps</Link>
-                </div>
+        <div className={styles.route}>
+          <Link to="/Contakt">Contakt</Link>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <label className={styles.switch}>
+          <input type="checkbox" onChange={handleChangeInput} value={isDark} />
+          <div className={styles.slider}></div>
+        </label>
 
-                 <div className={styles.route}>
-                   <Link to='/blog'>Blog-page</Link>
-                 </div>
-            
-                  <div className={styles.route}>
-                   <Link to='/Contakt'>Contakt</Link>
-                 </div>
-            </div>
-           <div className={styles.right}>
-                    
-                <label className={styles.switch}>
-                       <input type="checkbox"  onChange={handleChangeInput} value={isDark}/>
-                     <div className={styles.slider}></div>
-                </label>
-
-                  <div className={styles.route}>
-                      <Link  to='/accaund'> <UserOutlined />Accaund</Link>
-                 </div>
-                 </div>
-           </div>
-    );
-}
+        <div className={styles.route}>
+          <Link to="/accaund">
+            {" "}
+            <UserOutlined />
+            Accaund
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
--- src/Components/router/Router.js
+++ src/Components/router/Router.js
@@ -1,37 +1,33 @@
-import React from 'react';
-import { Routes, Route, Outlet, Link, BrowserRouter } from 'react-router-dom';
-import Home from '../Home/Home';
-import Products from '../Products/Products';
-import Basket from '../Basket/Basket';
-import Layout from '../layout/Layout';
-import styles from './Router.module.scss'
-import Carusel from '../Carusel/Carusel';
-import Blog from '../Blog/Blog'
-import Accounds from '../Accounds/Accounds';
-import Help from '../Help/Help';
-
-
+import React from "react";
+import { Routes, Route, Outlet, Link, BrowserRouter } from "react-router-dom";
+import Home from "../Home/Home";
+import Products from "../Products/Products";
+import Basket from "../Basket/Basket";
+import Layout from "../layout/Layout";
+import styles from "./Router.module.scss";
+import Carusel from "../Carusel/Carusel";
+import Blog from "../Blog/Blog";
+import Accounds from "../Accounds/Accounds";
+import Help from "../Help/Help";
 
 const Router = () => {
-    return (
-        <div className={styles.container}>
-                 
-                   <BrowserRouter>
-                <Routes>
-                        <Route path='/' element={<Layout />}>
-                        <Route index element={<Help />} />
-                        <Route path='products' element={<Products />} />
-                        <Route path='basket' element={<Basket />} />
-                        <Route path='carusel' element={<Carusel />} />
-                        <Route path='blog' element={<Blog />} />
-                       {/* <Route index element={<Accounds />} /> */}
-                        
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="help" element={<Help />} />
+            <Route path="products" element={<Products />} />
+            <Route path="basket" element={<Basket />} />
+            <Route path="carusel" element={<Carusel />} />
+            <Route path="blog" element={<Blog />} />
+            {/* <Route index element={<Accounds />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Router;
